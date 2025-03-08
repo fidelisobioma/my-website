28aecdfe61576d8d7c6606bfdaf0509af7175d6f
fidelisobioma <mbamfidelisobioma1@gmail.com>
2025-03-08 01:00:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const text = document.querySelector(".text");
+setTimeout(function () {
+  text.innerHTML = ` <h1>Hello World👋🏼</h1>
+      <p>Let me teach How to Host website Using GitHub pages</p>`;
+}, 5000);
